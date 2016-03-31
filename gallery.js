fd503fb87cd37c2d9e5cd6c0bc0e24aaa168eb8f
Alejandro Fernandez Barroso <abarroso@zeta40.aulas.gsyc.es>
2016-03-31 14:41:32 +0200

    Primera Version

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ function supports_history_api() {
 function swapPhoto(href) {
   var req = new XMLHttpRequest();
   req.open("GET",
-           "http://gsyc.es/~grex/history_api/gallery/" +
+           "gallery/" +
              href.split("/").pop(),
            false);
   req.send(null);
@@ -32,8 +32,10 @@ function setupHistoryClicks() {
 }
 
 window.onload = function() {
-  if (!supports_history_api()) { return; }
+  //if (!supports_history_api()) { return; }
+if (Modernizr.history) {
   setupHistoryClicks();
+}
   window.setTimeout(function() {
     window.addEventListener("popstate", function(e) {
       swapPhoto(location.pathname);
